d9a394a06140338f4a818ba78076da681bb42a20
Johannes Seitz <johannes.w.seitz@gmail.com>
2017-04-09 17:33:53 +0200

    Move from iron to nickel.rs

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,14 +1,16 @@
-extern crate iron;
 extern crate markdown;
 
 use std::path::{PathBuf, Path};
 use std::fs::File;
 use std::io::{Read,BufReader, Error};
+use std::collections::BTreeMap;
 
 use iron::prelude::*;
 use iron::{Url, Handler, status};
 use iron::modifiers::Redirect;
 use iron::headers::ContentType;
+
+use handlebars::Handlebars;
 
 pub struct IndexHandler {
     pub listening_port:i32,
@@ -30,12 +32,16 @@ impl Handler for IndexHandler {
 }
 
 pub struct MarkdownFileHandler {
-
+    handlebars:Handlebars
 }
 
 impl MarkdownFileHandler {
-    pub fn new() -> MarkdownFileHandler {
-        MarkdownFileHandler {}
+    pub fn new(theme:String) -> MarkdownFileHandler {
+        let mut handlebars = Handlebars::new();
+        if let Err(e) = handlebars.register_template_file("index", theme) {
+            panic!("{}", e);
+        }
+        MarkdownFileHandler {handlebars:handlebars}
     }
 }
 
@@ -44,9 +50,12 @@ impl Handler for MarkdownFileHandler {
         let markdown_file_path = map_request_url_path_to_local_file(req);
         let file_content = itry!(read_file_to_string(markdown_file_path.as_path()));
         let html : String = markdown::to_html(file_content.as_str());
-        Ok(Response::with((ContentType::html().0, status::Ok, html)))
-    }
 
+        let mut data = BTreeMap::new();
+        data.insert("markdown_content".to_string(), html);
+        let template = itry!(self.handlebars.render("index", &data));
+        Ok(Response::with((ContentType::html().0, status::Ok, template)))
+    }
 }
 
 fn read_file_to_string(path: &Path) -> Result<String, Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,53 @@
-#[macro_use] extern crate iron;
+#[macro_use] extern crate nickel;
 extern crate config;
-extern crate router;
+extern crate markdown;
 
-mod handlers;
+use nickel::Nickel;
 
 use std::path::Path;
 use std::collections::HashMap;
-
-use router::Router;
+use std::fs::File;
+use std::io::{Read,BufReader, Error};
 
 use config::reader;
 use config::types::Config;
-use iron::prelude::*;
 
-use handlers::{IndexHandler, MarkdownFileHandler};
-
+use nickel::*;
+use nickel::extensions::Redirect;
 
 fn main() {
     let empty_default_config = Config::new(HashMap::new());
     let configuration = reader::from_file(Path::new("Dokiofile")).unwrap_or(empty_default_config);
 
     let listening_port = configuration.lookup_integer32_or("port", 3000);
-    let default_file = configuration.lookup_str_or("default_file", "README.md");
+    let default_file = String::from(configuration.lookup_str_or("default_file", "README.md"));
+    let theme = String::from(configuration.lookup_str_or("theme", "themes/dokio/index.hbs"));
 
-    let mut router = Router::new();
-    router.get("/", IndexHandler::new(listening_port, String::from(default_file)), "index");
-    router.get("/*.md", MarkdownFileHandler::new(), "file");
+    let mut server = Nickel::new();
+    server.get("/", middleware! { |_, response|
+        let url_string = format!("http://localhost:{}/{}", listening_port, default_file.as_str());
+        return response.redirect(url_string);
+    });
+    server.get("**.md", middleware! { |request, response|
+        let file = request.path_without_query();
+        if let Ok(markdown_file) = read_file_to_string(file) {
+            let mut data = HashMap::new();
+            let markdown_as_html = markdown::to_html(markdown_file.as_str());
+            data.insert("markdown_file", markdown_as_html);
+            return response.render(theme.as_str(), &data);
+        }
+    });
+    server.utilize(Mount::new("/themes/", StaticFilesHandler::new("themes")));
 
-    let _server = Iron::new(router).http(format!("localhost:{}", listening_port).as_str()).unwrap();
+    let _server = server.listen(format!("localhost:{}", listening_port).as_str());
     println!("Dokio is running on port {}", listening_port);
 }
 
+fn read_file_to_string(path: Option<&str>) -> Result<String, Error> {
+    let file_path = format!(".{}", path.unwrap());
+    let file = File::open(Path::new(file_path.as_str()))?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    Ok(contents)
+}
